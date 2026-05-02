1a1746e452bb0bf580f53bb6fb1a75aa9c6faa80
xxadrianxx-bite <xxadrianbusinessxx@gmail.com>
2026-05-02 15:46:39 -0600

    build: limpieza de artefactos locales y configuracion final de Astro 5

--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_9h-1fp6B.mjs';
-export { renderers } from '../renderers.mjs';
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless'; // <--- Asegúrate que diga /serverless
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  output: 'server', // Esto es vital para que genere el entry.mjs
-  adapter: vercel(),
+  output: 'server', 
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+    imagesConfig: { sizes: [640, 750, 828, 1080, 1200], domains: [], },
+    // Forzamos que use la infraestructura estándar de Vercel
+    functionPerRoute: false 
+  }),
   integrations: [tailwind()],
 });

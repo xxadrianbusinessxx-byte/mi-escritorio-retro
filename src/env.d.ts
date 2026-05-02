2b8b68eddcb4ae2ff59b0babc479e26686b8693f
xxadrianxx-bite <xxadrianbusinessxx@gmail.com>
2026-05-02 17:24:07 -0600

    Cambiar el las variables CLOUDINARY_CLOUD_NAME por PUBLIC_CLOUDINARY_CLOUD_NAME

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="astro/client" />
 interface ImportMetaEnv {
-  readonly CLOUDINARY_CLOUD_NAME: string;
+  readonly PUBLIC_CLOUDINARY_CLOUD_NAME: string;
   readonly CLOUDINARY_API_KEY: string;
   readonly CLOUDINARY_API_SECRET: string;
 }
--- src/pages/api/images.json.ts
+++ src/pages/api/images.json.ts
@@ -2,7 +2,7 @@ import type { APIRoute } from 'astro';
 import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({
-  cloud_name: import.meta.env.CLOUDINARY_CLOUD_NAME,
+  cloud_name: import.meta.env.PUBLIC_CLOUDINARY_CLOUD_NAME,
   api_key: import.meta.env.CLOUDINARY_API_KEY,
   api_secret: import.meta.env.CLOUDINARY_API_SECRET,
 });
